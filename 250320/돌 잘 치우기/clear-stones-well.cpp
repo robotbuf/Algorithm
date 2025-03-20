a4f3d12f1d3412562d368e1ecb15a7a64f6a6e48
Robotca <gommj0611@gmail.com>
2025-03-20 18:13:59 +0900

    Title: 돌 잘 치우기, Time: 1064ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250320/돌 잘 치우기/clear-stones-well.cpp
+++ 250320/돌 잘 치우기/clear-stones-well.cpp
@@ -8,99 +8,80 @@ using namespace std;
 
 int n, k, m;
 int grid[100][100];
-int r[10000], c[10000];
+int r[10], c[10];
 
 vector<pair<int, int>> rock;
+vector<int> rs;
 
-int dx[4] = {-1,1,0,0};
-int dy[4] = {0,0,-1,1};
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
 
 bool isIn(int x, int y) {
-    return (0<=x && 0<=y && x<n && y<n);
+    return (0 <= x && 0 <= y && x < n && y < n);
 }
 
-
-int bfs(vector<int> li, int x, int y) {
-    queue<pair<int,int>> q;
+int bfs(vector<int> li) {
+    queue<pair<int, int>> q;
     bool visited[100][100] = {false};
-    
     int copyed[100][100];
-    memcpy(copyed,grid,sizeof(grid));
-    
+    memcpy(copyed, grid, sizeof(grid));
+
     int cnt = 0;
-    
-//    for(int i=0; i<li.size(); i++) {
-//        cout<<li[i]<< " ";
-//    }
-//    cout<<"\n";
-    
-    //사전작업
-    for(int i=0; i<li.size(); i++) {
+
+    for (int i = 0; i < li.size(); i++) {
         int x = rock[li[i]].first;
         int y = rock[li[i]].second;
-        
         copyed[x][y] = 0;
     }
-    
-    q.push({x,y});
-    visited[x][y] = true;
-    
-    while(!q.empty()) {
+
+    for (int i = 0; i < k; i++) {
+        q.push({r[i], c[i]});
+        visited[r[i]][c[i]] = true;
+    }
+
+    while (!q.empty()) {
         int x = q.front().first;
         int y = q.front().second;
         q.pop();
-        
+
         cnt++;
-        
-        for(int i=0; i<4; i++) {
+
+        for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            
-            if(isIn(nx,ny) && !visited[nx][ny]) {
-                if(copyed[nx][ny] == 0) {
-                    q.push({nx,ny});
-                    visited[nx][ny] = true;
-                }
+
+            if (isIn(nx, ny) && !visited[nx][ny] && copyed[nx][ny] == 0) {
+                q.push({nx, ny});
+                visited[nx][ny] = true;
             }
-            
         }
-        
-        
     }
-    
+
     return cnt;
 }
 
-
-vector<int> rs;
-
-
 int ans = 0;
-//bool check[100] = {false};
-void backtrack(int u) {
-    if(rs.size() == m) {
-        for(int i=0; i<k; i++) {
-            ans = max(ans,bfs(rs, r[i], c[i]));
-        }
+void backtrack(int start) {
+    if (rs.size() == m) {
+        ans = max(ans, bfs(rs));
         return;
     }
-    
-    for(int i=u; i<rock.size(); i++) {
-        
-        
+
+    for (int i = start; i < rock.size(); i++) {
         rs.push_back(i);
-        backtrack(i+1);
+        backtrack(i + 1);
         rs.pop_back();
-        
     }
 }
 
-
 int main() {
+    cin.tie(NULL);
     cin >> n >> k >> m;
 
     for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++) cin >> grid[i][j];
+        for (int j = 0; j < n; j++)
+            cin >> grid[i][j];
+
 
     for (int i = 0; i < k; i++) {
         cin >> r[i] >> c[i];
@@ -108,20 +89,19 @@ int main() {
         c[i]--;
     }
     
-    //돌 위치 찾자
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(grid[i][j] == 1) {
-                rock.push_back({i,j});
+  
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (grid[i][j] == 1) {
+                rock.push_back({i, j});
             }
         }
     }
 
-    // Please write your code here.
-    
+
     backtrack(0);
-    
-    cout<<ans;
+
+    cout << ans;
 
     return 0;
 }
