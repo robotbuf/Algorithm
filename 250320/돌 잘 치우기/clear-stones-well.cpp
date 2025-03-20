c0bdf89d507173c2cb6523d10cd8845005a8a1e2
Robotca <gommj0611@gmail.com>
2025-03-20 17:47:34 +0900

    Title: 돌 잘 치우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250320/돌 잘 치우기/clear-stones-well.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+int n, k, m;
+int grid[100][100];
+int r[10000], c[10000];
+
+vector<pair<int, int>> rock;
+
+int dx[4] = {-1,1,0,0};
+int dy[4] = {0,0,-1,1};
+
+bool isIn(int x, int y) {
+    return (0<=x && 0<=y && x<n && y<n);
+}
+
+
+int bfs(vector<int> li, int x, int y) {
+    queue<pair<int,int>> q;
+    bool visited[100][100] = {false};
+    
+    int copyed[100][100];
+    memcpy(copyed,grid,sizeof(grid));
+    
+    int cnt = 0;
+    
+//    for(int i=0; i<li.size(); i++) {
+//        cout<<li[i];
+//    }
+    
+    //사전작업
+    for(int i=0; i<li.size(); i++) {
+        int x = rock[li[i]].first;
+        int y = rock[li[i]].second;
+        
+        copyed[x][y] = 0;
+    }
+    
+    q.push({x,y});
+    visited[x][y] = true;
+    
+    while(!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        
+        cnt++;
+        
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(isIn(nx,ny) && !visited[nx][ny]) {
+                if(copyed[nx][ny] == 0) {
+                    q.push({nx,ny});
+                    visited[nx][ny] = true;
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    return cnt;
+}
+
+
+vector<int> rs;
+bool check[100];
+
+
+int ans = 0;
+void backtrack(int n) {
+    if(rs.size() == m) {
+        for(int i=0; i<k; i++) {
+            ans = max(ans,bfs(rs, r[i], c[i]));
+        }
+        return;
+    }
+    
+    for(int i=n; i<rock.size(); i++) {
+        if(!check[i]) {
+            check[i] = true;
+            rs.push_back(i);
+            backtrack(i+1);
+            rs.pop_back();
+            check[i] = false;
+        }
+    }
+}
+
+
+int main() {
+    cin >> n >> k >> m;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++) cin >> grid[i][j];
+
+    for (int i = 0; i < k; i++) {
+        cin >> r[i] >> c[i];
+        r[i]--;
+        c[i]--;
+    }
+    
+    //돌 위치 찾자
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(grid[i][j] == 1) {
+                rock.push_back({i,j});
+            }
+        }
+    }
+
+    // Please write your code here.
+    
+    backtrack(0);
+    
+    cout<<ans;
+
+    return 0;
+}
