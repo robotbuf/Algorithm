3d8af0c442f28195d13fca4249fccb6ef88de35e
Robotca <gommj0611@gmail.com>
2025-04-14 19:04:08 +0900

    Title: 가장 먼저 나오는 숫자, Time: 48ms, Memory: 1MB, Status: Passed - Codetree

--- 250414/가장 먼저 나오는 숫자/first-appear-number.cpp
+++ 250414/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -7,30 +7,21 @@ int arr[100000];
 int query[100000];
 
 int bin_search(int target) {
-    int left = 0;
-    int right = n-1;
-    
-    while(left <= right) {
+    int left = 0, right = n - 1;
+    int res = -1;
+
+    while (left <= right) {
         int mid = (left + right) / 2;
         
-        if(arr[mid] == target) {
-            while(1) {
-                mid -= 1;
-                if(arr[mid] != target) {
-                    mid += 1;
-                    break;
-                }
-            }
-            return mid+1;
-        }
-        
-        if(arr[mid] < target) {
+        if (arr[mid] < target) {
             left = mid + 1;
         } else {
+            if (arr[mid] == target) res = mid;
             right = mid - 1;
         }
     }
-    return -1;
+
+    return (res == -1) ? -1 : res + 1;
 }
 
 int main() {
