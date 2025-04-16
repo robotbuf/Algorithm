0123df2b15c294ab121e8cc8756ad2df70ce6de8
Robotca <gommj0611@gmail.com>
2025-04-16 11:21:20 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250416/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
+++ 250416/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -1,43 +1,54 @@
 #include <iostream>
 #include <algorithm>
+#include <climits>
+
 using namespace std;
 
-int m, a, b;
+long long m;
+long long a,b;
 
-int bin_search(int target) {
-    int left = 1;
-    int right = m;
-    int cnt = 1;
+int min_n = INT_MAX;
+int max_n = 0;
 
-    while (left <= right) {
-        int mid = left + (right - left) / 2;  // 내림 보장
-        if (mid == target) {
+int bin_search(long long target) {
+    long long left = 1;
+    long long right = m;
+    
+    int cnt = 0;
+    
+    while(left <= right) {
+        ++cnt;
+        long long mid = (left + right) / 2;
+        
+        if(mid == target) {
             break;
-        } else if (mid < target) {
+        } else if(mid < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
-        cnt++;
     }
-
+    
     return cnt;
 }
 
+
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    cin >> m >> a >> b;
-
-    int minTurn = 1e9;
-    int maxTurn = 0;
-
-    for (int i = a; i <= b; ++i) {
+    
+    cin>>m;
+    cin>>a>>b;
+    
+    for(long long i=a; i<=b; i++) {
         int cnt = bin_search(i);
-        minTurn = min(minTurn, cnt);
-        maxTurn = max(maxTurn, cnt);
+        min_n = min(cnt, min_n);
+        max_n = max(cnt, max_n);
     }
+    
+    
+    cout<<min_n<<" "<<max_n;
 
-    cout << minTurn << ' ' << maxTurn << '\n';
+    
+    
+    
+    
 }
