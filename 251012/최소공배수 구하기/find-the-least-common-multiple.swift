d7cf9349046aa9a7c9ce7a6dd0627c2b7740eacd
Robotca <gommj0611@gmail.com>
2025-10-12 11:38:58 +0900

    Title: 최소공배수 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251012/최소공배수 구하기/find-the-least-common-multiple.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+let input = readLine()!.components(separatedBy: " ")
+let n = Int(input[0])!
+let m = Int(input[1])!
+
+// Please write your code here.
+
+func gcd(_ a: Int, _ b: Int) -> Int {
+     var x = a 
+     var y = b 
+     
+     while(y != 0) {
+        int r = x % y 
+        x = y
+        y = r 
+     }
+
+     return x
+}
+
+print(n*m/gcd(n,m))
