dc41deba0fa66127bab327be11338c83e1181f88
Robotca <gommj0611@gmail.com>
2025-10-25 18:33:35 +0900

    Title: 자연수 n개의 합, Time: 13ms, Memory: 2MB, Status: Passed - Codetree

--- 251025/자연수 n개의 합/sum-of-n-natural-numbers.swift
+++ 251025/자연수 n개의 합/sum-of-n-natural-numbers.swift
@@ -1,16 +1,19 @@
 import Foundation
-let s = Int64(readLine()!)!
 
+let s = Int64(readLine()!)!   // 입력값 최대 10^18
 
 var left: Int64 = 0
-var right: Int64 = s
+var right: Int64 = 2_000_000_000  // n이 약 1.4e9 수준이라 이 정도면 충분
 var ans: Int64 = 0
 
 while left <= right {
     let mid = (left + right) / 2
-    let sum = mid * (mid + 1) / 2  // O(1) 계산
-
-    if sum <= s {
+    
+    // mid*(mid+1)/2 계산 시 오버플로우 방지
+    // → 중간 연산은 Int128처럼 처리할 수 없으니 Double로 한 번 변환
+    let sum = Double(mid) * Double(mid + 1) / 2.0
+    
+    if sum <= Double(s) {
         ans = mid
         left = mid + 1
     } else {
