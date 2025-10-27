2938b48c5e7d1e472a73cb9b1d548ffbe59ef903
Robotca <gommj0611@gmail.com>
2025-10-27 22:07:15 +0900

    Title: 비를 피하기, Time: 1086ms, Memory: 2MB, Status: Time Limit Exceed - Codetree

--- 251027/비를 피하기/stay-out-of-rain.swift
+++ 251027/비를 피하기/stay-out-of-rain.swift
@@ -24,11 +24,12 @@ for i in 0..<n {
 let dx = [-1,1,0,0]
 let dy = [0,0,-1,1]
 
-var ans = Array(repeating: Array(repeating: Int.max, count: m), count: n)
+var ans = Array(repeating: Array(repeating: 0, count: n), count: n)
+var checked = Array(repeating: Array(repeating: 0, count: n), count: n)
 
 func bfs(_ a: Int, _ b: Int) {
     var q: [(Int,Int,Int)] = []
-    var visited = Array(repeating: Array(repeating: false, count: m), count: n)
+    var visited = Array(repeating: Array(repeating: false, count: n), count: n)
 
     q.append((a,b,0))
     visited[a][b] = true
@@ -40,14 +41,19 @@ func bfs(_ a: Int, _ b: Int) {
         let cnt = node.2 
 
         if grid[x][y] == 2 {
-            ans[x][y] = min(ans[x][y], cnt)
+            if ans[x][y] == 0 {
+                ans[x][y] = cnt 
+            } else {
+                ans[x][y] = min(ans[x][y],cnt)
+            }
+            checked[x][y] = 2 
         }
         
         for i in 0..<4 {
             let nx = x + dx[i]
             let ny = y + dy[i]
 
-            if 0<=nx && nx<n && 0<=ny && ny<m && !visited[nx][ny] {
+            if 0<=nx && nx<n && 0<=ny && ny<n && !visited[nx][ny] {
                 if grid[nx][ny] != 1 {
                     q.append((nx,ny,cnt+1))
                     visited[nx][ny] = true 
@@ -57,13 +63,30 @@ func bfs(_ a: Int, _ b: Int) {
 
     }
 }
+for i in 0..<n {
+    for j in 0..<n {
+        if grid[i][j] == 2{
+            checked[i][j] = 1
+        }
+    }
+}
 
 for i in 0..<n {
-    for j in 0..<m {
+    for j in 0..<n {
         if grid[i][j] == 3 {
             bfs(i,j)
         }
     }
 }
 
-print(ans)
+for i in 0..<n {
+    for j in 0..<n {
+        if(checked[i][j] == 1) {
+            ans[i][j] = -1
+        }
+    }
+}
+
+for row in ans {
+    print(row.map(String.init).joined(separator: " "))
+}
